85397036ddeba934a092d7cc4735c7cb90a3fab8
DylanAaronElicserioMartinez <a.23308051281179@cbtis128.edu.mx>
2024-09-23 20:24:56 -0600

    Add files via upload

---
+++ Calculo de promedios.py
@@ -0,0 +1,20 @@
+print(" ")
+print("Dylan Aaron Elicserio Martínez esta es mi practica:")
+print(" ")
+#Solicitar al usuario que ingrese el primer numero para sacar el promedio
+numero1=input("ingresa el primer numero:")
+#Conversion del primer numero caracter entero
+num1=int(numero1)
+#Solicitar al usuario que ingrese el sugundo numero para sacar el promedio
+numero2=input("ingresa el segundo numero:")
+#Conversion del segundo numero a caracter entero
+num2=int(numero2)
+#Solicita al usuario que ingrese el tercer numero para sacar el promedio
+numero3=input("ingresa el tercer numero:")
+#Conversion del tercer numero a caracter entero
+num3=int(numero3)
+#saca el promedio de los numeros
+promedio= num1 /3 + num2 /3  + num3 /3
+#Muestra el promedio de los numeros
+resultado=print("El Resultado es:")
+print(promedio)
